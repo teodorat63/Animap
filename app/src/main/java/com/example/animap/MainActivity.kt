611b7b18193a49ac55b5d45d81d66204ef3cf21b
teodorat63 <teodoratomic300@gmail.com>
2024-07-25 14:43:08 +0200

    Moved registration logic to RegisterActivity

--- app/src/main/java/com/example/animap/MainActivity.kt
+++ app/src/main/java/com/example/animap/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.animap
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.animap.screens.HomeScreen.RegistrationScreen
 import com.example.animap.ui.theme.AnimapTheme
 import com.google.firebase.auth.FirebaseAuth
@@ -24,10 +35,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    RegistrationScreen()
+                    MainActivityContent()
                 }
             }
         }
     }
 }
 
+@Composable
+fun MainActivityContent() {
+    val ctx = LocalContext.current
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Button(onClick = {
+            val toRegister = Intent(ctx, RegisterActivity::class.java)
+            ctx.startActivity(toRegister)
+        }) {
+            Text(text = "Register")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Log in")
+        }
+    }
+
+}
