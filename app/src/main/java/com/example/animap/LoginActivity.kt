2aa5c72dfa7e8a5b58b870b224d2687edbe7e809
teodorat63 <teodoratomic300@gmail.com>
2024-07-25 15:37:48 +0200

    Added login functionality

--- app/src/main/java/com/example/animap/LoginActivity.kt
+++ app/src/main/java/com/example/animap/LoginActivity.kt
@@ -1,6 +1,5 @@
 package com.example.animap
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -15,18 +14,21 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.animap.screens.HomeScreen.RegistrationScreen
 import com.example.animap.ui.theme.AnimapTheme
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContent {
             AnimapTheme {
@@ -35,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainActivityContent()
+                    LoginScreenContent()
                 }
             }
         }
@@ -43,24 +45,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainActivityContent() {
-    val ctx = LocalContext.current
+fun LoginScreenContent() {
+    val auth = FirebaseAuth.getInstance()
+    val context = LocalContext.current
+
+    val email = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        TextField(
+            value = email.value,
+            onValueChange = { email.value = it },
+            label = { Text("Email") }
+        )
+        TextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
         Button(onClick = {
-            val toRegister = Intent(ctx, RegisterActivity::class.java)
-            ctx.startActivity(toRegister)
+            auth.signInWithEmailAndPassword(email.value, password.value)
+                .addOnCompleteListener{ task ->
+                    if (task.isSuccessful) {
+                        // Login success, navigate to the next screen
+                        Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+                        // startActivity(Intent(this, NextActivity::class.java))
+                        // finish()
+                    } else {
+                        // If login fails, display a message to the user.
+                        Toast.makeText(context, "Authentication Failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }) {
-            Text(text = "Register")
-        }
-        Button(onClick = { val toLogin = Intent(ctx, LoginActivity::class.java)
-            ctx.startActivity(toLogin) }) {
-            Text(text = "Log in")
+            Text("Log in")
         }
     }
-
 }
